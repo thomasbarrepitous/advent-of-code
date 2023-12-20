4bfd3cc44180788737981a2b5b3c81755f44deac
Thomas Barré-Pitous <barrepitousthomas@gmail.com>
2023-12-20 21:30:48 +0900

    Day 9 part 1

---
+++ 2023/9/9_1.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func isHistoryOnlyZero(history []int) bool {
+	for _, h := range history {
+		if h != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func computeNextValue(history []int) int {
+	processedHistory := make([]int, len(history)-1)
+	for i := 0; i < len(history)-1; i++ {
+		firstTerm := history[i]
+		secondTerm := history[i+1]
+		processedHistory[i] = secondTerm - firstTerm
+	}
+	fmt.Println(processedHistory)
+	if isHistoryOnlyZero(processedHistory) {
+		return 0
+	}
+	return computeNextValue(processedHistory) + processedHistory[len(processedHistory)-1]
+}
+
+func main() {
+	// Specify the path to your text file
+	filePath := "input.txt"
+
+	// Open the file
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Create a slice to store the lines
+	var lines []string
+
+	// Create a scanner to read the file line by line
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	// Check for scanner errors
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	var flag int = 0
+	for _, line := range lines {
+		var history []int
+		fmt.Println(line)
+		linesSplit := strings.Split(line, " ")
+		for _, value := range linesSplit {
+			parsedValue, err := strconv.Atoi(value)
+			if err != nil {
+				fmt.Println("Error parsing value:", err)
+				return
+			}
+			history = append(history, parsedValue)
+		}
+		flag += history[len(history)-1] + computeNextValue(history)
+	}
+
+	fmt.Println("flag:", flag)
+}

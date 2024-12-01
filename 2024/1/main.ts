81739314136e1d7a172549e13bcb5b3284343673
Thomas <barrepitousthomas@gmail.com>
2024-12-01 21:08:57 +0100

    1st day

--- 2024/1/main.ts
+++ 2024/1/main.ts
@@ -10,16 +10,54 @@ function parseInput(filename: string): Input {
         .split('\n');
 }
 
+function parseInputAsNumbers(input: Input): [number[], number[]] {
+    const column1: number[] = [];
+    const column2: number[] = [];
+
+    input.forEach(line => {
+        const [num1, num2] = line.trim().split(/\s+/).map(Number);
+        column1.push(num1);
+        column2.push(num2);
+    });
+
+    return [column1, column2];
+}
+
+function findDistance(column1: number[], column2: number[]): number {
+    let distance = 0;
+    for (let i = 0; i < column1.length; i++) {
+        distance += Math.abs(column1[i] - column2[i]);
+    }
+    return distance;
+}
+
+function findSimilarity(column1: number[], column2: number[]): number {
+    // The similarity is the number of occurences of the left column number in the right column
+    let similarity = 0;
+    for (let i = 0; i < column1.length; i++) {
+        similarity += column1[i] * column2.filter(num => num === column1[i]).length;
+        console.log(column1[i], similarity);
+    }
+    return similarity;
+}
+
 // Part 1 solution
 function solvePart1(input: Input): number {
-    // TODO: Implement part 1 solution
-    return 0;
+    const [column1, column2] = parseInputAsNumbers(input);
+    // Sort both columns
+    column1.sort((a, b) => a - b);
+    column2.sort((a, b) => a - b);
+
+    // Find the distance between the two columns
+    return findDistance(column1, column2);
 }
 
 // Part 2 solution
 function solvePart2(input: Input): number {
-    // TODO: Implement part 2 solution
-    return 0;
+    const [column1, column2] = parseInputAsNumbers(input);
+
+    // Find the similarity between the two columns
+    return findSimilarity(column1, column2);
 }
 
 // Main execution
